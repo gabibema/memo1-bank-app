96dadd67850b9d2e364012051fec4be38715c7c2
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2022-11-06 21:39:08 -0300

    añadiendo promociones

--- src/main/java/com/aninfo/service/TransactionService.java
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -1,5 +1,7 @@
 package com.aninfo.service;
 
+import com.aninfo.exceptions.DepositNegativeSumException;
+import com.aninfo.exceptions.InsufficientFundsException;
 import com.aninfo.model.Transaction;
 import com.aninfo.model.TransactionType;
 import com.aninfo.repository.TransactionRepository;
@@ -22,13 +24,30 @@ public class TransactionService {
     public Transaction createTransaction(Transaction transaction) {
 
         if (transaction.getType() == TransactionType.DEPOSIT) {
+            if (transaction.getSum() <= 0){
+                throw new DepositNegativeSumException("Cannot deposit negative sum");
+            }
+
+            transaction.setSum(this.applyPromo(transaction.getSum()));
             accountService.deposit(transaction.getCbu(), transaction.getSum());
 
         } else if (transaction.getType() == TransactionType.WITHDRAWAL) {
+            if (transaction.getSum() > accountService.findById(transaction.getCbu()).get().getBalance()){
+                throw new InsufficientFundsException("Cannot withdraw over balance");
+            }
             accountService.withdraw(transaction.getCbu(), transaction.getSum());
         }
         
         return transactionRepository.save(transaction);
+    }
+
+    private Double applyPromo(Double sum){
+        if (sum >= 2000){
+            Double promotional = sum * 0.1;
+            if (promotional > 500) promotional = 500.00;
+            sum += promotional;
+        }
+        return sum;
     }
     
     public Collection<Transaction> getTransactions(long cbu) {
